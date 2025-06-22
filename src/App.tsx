eb2d0ab2e7b78e39c2b2091e886571e1f5e2212d
Immanuel Sohn <45334065+immnlshn@users.noreply.github.com>
2025-06-22 18:18:47 +0200

    Remove tailwind and unused CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
-import './App.css'
+import * as stylex from '@stylexjs/stylex'
 import Quiz from './components/Quiz'
 
+const styles = stylex.create({
+  app: {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '2rem',
+  },
+})
+
 export default function App() {
-  return <Quiz />
+  return (
+    <div {...stylex.props(styles.app)}>
+      <Quiz />
+    </div>
+  )
 }
